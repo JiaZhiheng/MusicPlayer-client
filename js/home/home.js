40c0eeabc43512f972c0884f33c5df114a810c15
贾志恒 <1263515884@qq.com>
2022-02-22 09:53:22 +0800

    实现首页推荐歌单功能

--- js/home/home.js
+++ js/home/home.js
@@ -21,17 +21,85 @@ const homePageTemplate = `
     </div>
 </div>
 `;
-//首页初始化
-document.querySelector("#app").innerHTML = homePageTemplate;
+
 
 import { getBannerList } from "../service/ajax.js";
-
-const result = await getBannerList();
-const carouselData = result.data.blocks[0].extInfo.banners;
-
 import { carouselRender, initCarouselEvent } from "./carousel.js";
 
+function recommendRender(data) {
+  //获得推荐歌单盒子
+  const recommendWrapper = document.querySelector(
+    ".recommend-playlist-container"
+  );
+  let template = "";
+  let length = data.length;
+  data.forEach((item, index) => {
+    // 此处相较于实验2 home.html 中有添加一个 a 标签包裹图片和文字，目的是用来完成页面跳转，达到单页面应用的目的
+    template += `
+              <li data-index=${index} class="recommend-playlist-item d-flex flex-column }" style="width:${
+        98 / length
+      }%">
+                  <div class="recommend-playlist-cover">
+                      <a href='#/recommendList/:${item.creativeId}'>
+                          <img src="${item.uiElement.image.imageUrl}"
+                              alt="">
+                          <svg class="recommend-playlist-icon icon" aria-hidden="true">
+                              <use xlink:href="#icon-zanting"></use>
+                          </svg>
+                      </a>
+                  </div>
+                  <div class="recommend-playlist-title multi-text-omitted">
+                      ${item.uiElement.mainTitle.title}
+                  </div>
+              </li>
+              `;
+  });
+  recommendWrapper.innerHTML = template;
+}
+
+function initRecommendEvent() {
+  //动态增加 hover 类
+  const recommendWrapper = document.querySelector(
+    ".recommend-playlist-container"
+  );
+  recommendWrapper.addEventListener(
+    "mouseenter",
+    (e) => {
+      if (e.target.tagName === "LI") {
+        e.target.setAttribute(
+          "class",
+          "recommend-playlist-item d-flex flex-column hover"
+        );
+      }
+    },
+    true
+  );
+  recommendWrapper.addEventListener(
+    "mouseleave",
+    (e) => {
+      if (e.target.tagName === "LI") {
+        e.target.setAttribute(
+          "class",
+          "recommend-playlist-item d-flex flex-column "
+        );
+      }
+    },
+    true
+  );
+}
+
+//首页初始化
+document.querySelector('#app').innerHTML = homePageTemplate;
+const result = await getBannerList();
+console.log("🚀 ~ file: home.js ~ line 76 ~ homePage ~ result", result)
+const carouselData = result.data.blocks[0].extInfo.banners;
 //首次渲染轮播图
 carouselRender(carouselData);
 //轮播图事件绑定
 initCarouselEvent();
+
+const recommendData = [...result.data.blocks[1].creatives];
+// 初始化歌单推荐列表
+recommendRender(recommendData);
+// 初始化页面事件
+initRecommendEvent()
